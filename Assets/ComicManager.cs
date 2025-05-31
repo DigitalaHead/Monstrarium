880908a888f933a4adca8415306be9775e80fa11
AvocadoU <ilya.gorodnichev.04@mail.ru>
2025-05-31 23:41:13 +0500

    YG

--- Assets/ComicManager.cs
+++ Assets/ComicManager.cs
@@ -3,26 +3,31 @@ using UnityEngine.UI;
 
 public class ComicManager : MonoBehaviour
 {
-    public GameObject comicPanel; // Панель с комиксом
-    public Image[] comicImages;   // Массив картинок-комиксов
+    public GameObject comicPanel;      // Панель с комиксом
+    public Image[] comicImages;        // Картинки комикса
+    public GameObject[] tutorialObjects; // Объекты обучения
 
     private int currentIndex = 0;
     private bool comicActive = false;
+    private bool tutorialActive = false;
 
     void Start()
     {
         Debug.Log(PlayerPrefs.GetInt("ComicShown", 0));
+      //  PlayerPrefs.SetInt("ComicShown", 0); // Сброс для тестирования, удалить в релизе
         if (PlayerPrefs.GetInt("ComicShown", 0) == 0)
         {
             comicPanel.SetActive(true);
-            ShowImage(0);
+            ShowComicImage(0);
             Time.timeScale = 0;
             comicActive = true;
+            tutorialActive = false;
         }
         else
         {
             comicPanel.SetActive(false);
             comicActive = false;
+            tutorialActive = false;
         }
     }
 
@@ -30,35 +35,69 @@ public class ComicManager : MonoBehaviour
     {
         if (comicActive && Input.anyKeyDown)
         {
-            NextImage();
+            NextComicImage();
+        }
+        else if (tutorialActive && Input.anyKeyDown)
+        {
+            NextTutorialObject();
         }
     }
 
-    void ShowImage(int index)
+    void ShowComicImage(int index)
     {
         for (int i = 0; i < comicImages.Length; i++)
             comicImages[i].gameObject.SetActive(i == index);
+        // Скрыть все tutorialObjects
+        foreach (var obj in tutorialObjects)
+            obj.SetActive(false);
     }
 
-    void NextImage()
+    void ShowTutorialObject(int index)
+    {
+        for (int i = 0; i < tutorialObjects.Length; i++)
+            tutorialObjects[i].SetActive(i == index);
+        // Скрыть все comicImages
+        foreach (var img in comicImages)
+            img.gameObject.SetActive(false);
+    }
+
+    void NextComicImage()
     {
         currentIndex++;
         if (currentIndex < comicImages.Length)
         {
-            ShowImage(currentIndex);
+            ShowComicImage(currentIndex);
         }
         else
         {
-            CloseComic();
+            // Переход к обучению
+            currentIndex = 0;
+            comicActive = false;
+            tutorialActive = true;
+            ShowTutorialObject(0);
         }
     }
 
-    public void CloseComic()
+    void NextTutorialObject()
+    {
+        currentIndex++;
+        if (currentIndex < tutorialObjects.Length)
+        {
+            ShowTutorialObject(currentIndex);
+        }
+        else
+        {
+            CloseAll();
+        }
+    }
+
+    void CloseAll()
     {
         comicPanel.SetActive(false);
         Time.timeScale = 1;
         PlayerPrefs.SetInt("ComicShown", 1);
         PlayerPrefs.Save();
         comicActive = false;
+        tutorialActive = false;
     }
 }
